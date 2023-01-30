4f51fd6543f65829acf6e8d3b5854aff73de6833
samm-o <samuilsgeorgiev@gmail.com>
2023-01-30 09:58:48 -0500

    Add project files.

---
+++ File Text Displayer/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace File_Text_Displayer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var path = @"Insert txt file pathway here";
+            var words = File.ReadAllText(path).Split(" ");
+            var numOfWords = words.Count();
+            Console.WriteLine(numOfWords);
+
+        }
+    }
+}
